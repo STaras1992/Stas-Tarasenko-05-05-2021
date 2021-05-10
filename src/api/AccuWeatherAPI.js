eb9ca21806f3c3e8be8fb0dfa5564471dd71562e
Stas <stas23061992@gmail.com>
2021-05-10 10:00:15 +0300

    http to https return

--- src/api/AccuWeatherAPI.js
+++ src/api/AccuWeatherAPI.js
@@ -1,25 +1,25 @@
 import axios from 'axios';
 
 export const getLocationAutocomplete = async (text) => {
-  return await axios.get('http://dataservice.accuweather.com/locations/v1/cities/autocomplete', {
+  return await axios.get('https://dataservice.accuweather.com/locations/v1/cities/autocomplete', {
     params: { apikey: process.env.REACT_APP_WEATHER_API_KEY, q: text },
   });
 };
 
 export const getLocationByCoordinates = async (coordinates) => {
-  return await axios.get('http://dataservice.accuweather.com/locations/v1/cities/geoposition/search', {
+  return await axios.get('https://dataservice.accuweather.com/locations/v1/cities/geoposition/search', {
     params: { apikey: process.env.REACT_APP_WEATHER_API_KEY, q: `${coordinates.lat},${coordinates.lon}` },
   });
 };
 
 export const getCurrentWeather = async (locationKey) => {
-  return await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}`, {
+  return await axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${locationKey}`, {
     params: { apikey: process.env.REACT_APP_WEATHER_API_KEY },
   });
 };
 
 export const getFiveDayForecast = async (locationKey) => {
-  return await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}`, {
+  return await axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}`, {
     params: { apikey: process.env.REACT_APP_WEATHER_API_KEY },
   });
 };
